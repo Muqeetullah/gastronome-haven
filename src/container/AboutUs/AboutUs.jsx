c360aa62c646cd24d4bf62450df358e3fc7a7c07
Muqeetullah <87570384+Muqeetullah@users.noreply.github.com>
2024-01-15 14:38:19 +0500

    Added Chef , Gallery,Special Menu,Laurels, and footer

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -11,7 +11,12 @@ const AboutUs = () => (
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant"> About Us</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <img
+          src={images.spoon}
+          alt="about_spoon"
+          className="spoon__img "
+          style={{transform: "scaleX(-1)"}}
+        />
         <p className="p__opensans">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
           adipiscing ultrices vulputate posuere tristique. In sed odio nec
@@ -25,7 +30,7 @@ const AboutUs = () => (
         {" "}
         <img src={images.knife} alt="knife" />
       </div>
-      <div className="app__aboutus-content_history flex__center">
+      <div className="app__aboutus-content_history ">
         <h1 className="headtext__cormorant"> Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
         <p className="p__opensans">
